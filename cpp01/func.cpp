5c38d2c95d3df59de4620f02ffd02ab640c605c9
liquidaqua <diracookie@gmail.com>
2019-05-15 14:16:16 +0900

    git add

---
+++ cpp01/func.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+void output(int a)
+{
+    std::cout << "The answer is " << a << std::endl;
+}
